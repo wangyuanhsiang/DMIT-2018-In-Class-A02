2a4e5f0c1c1dc0055dceabc9e0aa2e50808b214a
wangyuanhsiang <ywang91@studentmail.nait.ca>
2015-09-23 02:12:02 -0600

    Create a new solution "eRestaurant Sample Demo"

---
+++ eRestaurant Sample Demo/eRestaurantSystem/Entities/SpecialEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace eRestaurantSystem.Entities
+{
+    public class SpecialEvent
+    {
+        [Key]
+        public string EventCode { get; set; }
+        public string Description { get; set; }
+        public Boolean Active { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
