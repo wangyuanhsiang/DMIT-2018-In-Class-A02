daf0e7f30d3b6eeb86b71b9e102a2237fa019f21
wangyuanhsiang <ywang91@studentmail.nait.ca>
2015-10-01 13:04:51 -0600

    Modify eRestaurant

--- eRestaurant Demo/eRestaurant.Framework/BLL/TempController.cs
+++ eRestaurant Demo/eRestaurant.Framework/BLL/TempController.cs
@@ -10,15 +10,38 @@ using eRestaurant.Framework.DAL;
 namespace eRestaurant.Framework.BLL
 {
      [DataObject]
+
      public class TempController
-    {
+     {
+         #region manage special events
          [DataObjectMethod(DataObjectMethodType.Select, false)]
          public List<SpecialEvent> GetSpecialEvent()
          {
              using (RestaurantContext specialEventDBContext = new RestaurantContext())
              { return specialEventDBContext.SpecialEvents.ToList(); }
          }
-             
 
-    }
+
+         [DataObjectMethod(DataObjectMethodType.Insert, false)]
+         public void AddSpecialEvent(SpecialEvent Entities)
+         {
+             using (RestaurantContext specialEventDBContext = new RestaurantContext())
+             { var adding = specialEventDBContext.SpecialEvents.Add(Entities); specialEventDBContext.SaveChanges(); }
+         }
+
+         [DataObjectMethod(DataObjectMethodType.Update, false)]
+         public void UpdateSpecialEvent (SpecialEvent Entities)
+         {       
+            using(RestaurantContext specialEventDBContext = new RestaurantContext())
+            { var updating = specialEventDBContext.SpecialEvents.Attach(Entities);
+              var matchingWithExistingValues = 
+            }
+         
+         }
+
+         #endregion
+
+     }
+
+
 }
